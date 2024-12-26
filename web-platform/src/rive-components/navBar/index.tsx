c835b766c2589b242c7ce90d870faa7a582a50fe
Ibrahim Azzam <ibrazzam7@gmail.com>
2024-12-26 18:01:38 +0300

    added functionality for Events and Store in NavBar

--- web-platform/src/rive-components/navBar/index.tsx
+++ web-platform/src/rive-components/navBar/index.tsx
@@ -1,8 +1,9 @@
 'use client';
-import { useRive, Layout, Fit, decodeFont } from '@rive-app/react-canvas';
+import { useRive, Layout, Fit, decodeFont, EventType, RiveEventType } from '@rive-app/react-canvas';
+import {  useEffect } from 'react';
 
 export const RiveNavBar = ({}) => {
-  const { RiveComponent } = useRive({
+  const {rive, RiveComponent } = useRive({
     src: '/rive/hero/hero.riv',
     artboard: 'Nav Bar',
     stateMachines: 'Nav Bar SM',
@@ -23,10 +24,28 @@ export const RiveNavBar = ({}) => {
         return false;
       }
     },
+    
   });
 
-  return <RiveComponent style={{ width: '100%', height: '80px' }} />;
+const onRiveEventReceived = (riveEvent : any) => {
+  const eventData = riveEvent.data;
+  console.log(eventData);
+  const eventProperties = eventData.properties;
+  if (eventData.type === RiveEventType.General && eventData.name === "Events") {
+    // window.location.href = "/events";
+  } else if (eventData.type === RiveEventType.General && eventData.name === "Store") {
+    window.location.href = "/store";
+  }
 };
+
+// Wait until the rive object is instantiated before adding the Rive
+// event listener
+useEffect(() => {
+  if (rive) {
+    rive.on(EventType.RiveEvent, onRiveEventReceived);
+  }
+}, [rive]);
+
 const fontAsset = (asset: any) => {
   fetch('/rive/font/Inter-594377.ttf').then(async (res) => {
     // decodeFont creates a Rive-specific Font object that `setFont()` takes
@@ -40,6 +59,9 @@ const fontAsset = (asset: any) => {
   });
 };
 
+return <RiveComponent style={{ width: '100%', height: '80px' }} />;
+
+};
 export default function App() {
   return (
     <div>
