a2004b3134520d831524778ec8bede0090338614
Mohammad Rahal <moodyrahal@gmail.com>
2025-01-07 18:58:04 +0200

    latest update

--- web-platform/src/app/layout.tsx
+++ web-platform/src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
 
         <Footer />
 
-        <div className="md:hidden bg-black fixed bottom-0 w-full z-10000">
+        <div className="md:hidden bg-black fixed bottom-0 w-full z-50">
           <RiveNavBar />
         </div>
       </body>
